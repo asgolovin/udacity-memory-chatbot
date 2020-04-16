b3aa54d01088604cb676ee634ff7bb53e09650c2
Anastasia Golovin <asgolovin@protonmail.com>
2020-04-16 21:36:56 +0200

    Give each GraphNode ownership of outgoing edges

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -1,64 +1,52 @@
-#include "graphedge.h"
 #include "graphnode.h"
+#include "graphedge.h"
 #include <iostream>
 
-GraphNode::GraphNode(int id)
-{
-    std::cout << "GraphNode Constructor\n";
-    _id = id;
+GraphNode::GraphNode(int id) {
+  std::cout << "GraphNode Constructor\n";
+  _id = id;
 }
 
-GraphNode::~GraphNode()
-{
-    //// STUDENT CODE
-    ////
-    std::cout << "GraphNode Destructor\n";
-    for (auto edge : _childEdges){
-        delete edge;
-    }
+GraphNode::~GraphNode() {
+  //// STUDENT CODE
+  ////
 
-    ////
-    //// EOF STUDENT CODE
+  std::cout << "GraphNode Destructor\n";
+
+  ////
+  //// EOF STUDENT CODE
 }
 
-void GraphNode::AddToken(std::string token)
-{
-    _answers.push_back(token);
+void GraphNode::AddToken(std::string token) { _answers.push_back(token); }
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge) {
+  _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
-{
-    _parentEdges.push_back(edge);
-}
-
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
-{
-    _childEdges.push_back(edge);
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge) {
+  _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot *chatbot)
-{
-    _chatBot = chatbot;
-    _chatBot->SetCurrentNode(this);
+void GraphNode::MoveChatbotHere(ChatBot *chatbot) {
+  _chatBot = chatbot;
+  _chatBot->SetCurrentNode(this);
 }
 
-void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
-{
-    newNode->MoveChatbotHere(_chatBot);
-    _chatBot = nullptr; // invalidate pointer at source
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode) {
+  newNode->MoveChatbotHere(_chatBot);
+  _chatBot = nullptr; // invalidate pointer at source
 }
 ////
 //// EOF STUDENT CODE
 
-GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
-{
-    //// STUDENT CODE
-    ////
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index) {
+  //// STUDENT CODE
+  ////
 
-    return _childEdges[index];
+  return (_childEdges[index]).get();
 
-    ////
-    //// EOF STUDENT CODE
+  ////
+  //// EOF STUDENT CODE
 }
